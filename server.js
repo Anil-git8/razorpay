a84d0e0e1cb44e503cb183a52e386a916b31af69
folk-connect <folkconnect.help@gmail.com>
2026-03-19 11:15:52 +0530

    set PORT to a fixed value of 3000 in server.js

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ app.post("/create-order", async (req, res) => {
 // -----------------------------
 // SERVER
 // -----------------------------
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`⚡ Server running in ${Date.now() - global.startTime}ms`);
